f4b99c50afd0a8446b0d7bc496178612b38db667
Belula Belai <belulaaab@hotmail.com>
2023-01-26 18:41:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, set, remove, onChildAdded, onChildRemoved } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, set, onChildAdded, } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 
 const firebaseConfig = {
