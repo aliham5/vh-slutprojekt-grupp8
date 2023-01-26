ed33592a6501dfbaeecdf55014044b7408089a81
Ali Hamze <ali_hamze@hotmail.com>
2023-01-26 18:43:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,33 +27,6 @@ if (sessionStorage.getItem('sender')) {
 };
 
 
-// TO SEND MESSAGES
-const sendMsg = function sendMsg() {
-   var msg = msgTxt.value;
-   var timestamp = new Date().getTime();
-   set(ref(db, "messages/" + timestamp), {
-      msg: msg,
-      sender: sender
-   })
-
-
-   msgTxt.value = "";
-};
-
-
-document.getElementById("msgBtn").addEventListener("click", sendMsg);
-
-
-
-// TO RECEIVE MSG
-onChildAdded(ref(db, "messages"), (data) => {
-   if (data.val().sender == sender) {
-      messages.innerHTML += "<div style=justify-content:end class=outer id=" + data.key + "><div id=inner class=me>you : " + data.val().msg + "<button id=dltMsg onclick=module.dltMsg(" + data.key + ")>DELETE</button></div></div>";
-   } else {
-      messages.innerHTML += "<div class=outer id=" + data.key + "><div id=inner class=notMe>" + data.val().sender + " : " + data.val().msg + "</div></div>";
-   }
-});
-
 
 
 
