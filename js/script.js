d4430fb856d5190294b8d82bc73a95d326507017
Ali Hamze <ali_hamze@hotmail.com>
2023-01-26 18:55:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, set, onChildAdded, } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, set, remove, onChildAdded, onChildRemoved } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 
 const firebaseConfig = {
@@ -11,6 +11,9 @@ const firebaseConfig = {
    appId: "1:253984871721:web:4560475db8498f9e7bd338",
    measurementId: "G-8WTXPLYEGV"
 };
+
+
+
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
@@ -24,7 +27,7 @@ if (sessionStorage.getItem('sender')) {
 } else {
    sender = prompt('PLEASE ENTER YOUR NAME');
    sessionStorage.setItem('sender', sender);
-};
+}
 
 
 // TO SEND MESSAGES
@@ -32,38 +35,27 @@ const sendMsg = function sendMsg() {
    var msg = msgTxt.value;
    var timestamp = new Date().getTime();
    set(ref(db, "messages/" + timestamp), {
-      msg: msg,
-      sender: sender
+       msg: msg,
+       sender: sender
    })
 
 
    msgTxt.value = "";
-};
+}
 
 
-document.getElementById("msgBtn").addEventListener("click", sendMsg);
+document.getElementById("msgBtn").addEventListener("click",sendMsg);
 
 
 
 // TO RECEIVE MSG
 onChildAdded(ref(db, "messages"), (data) => {
    if (data.val().sender == sender) {
-      messages.innerHTML += "<div style=justify-content:end class=outer id=" + data.key + "><div id=inner class=me>you : " + data.val().msg + "<button id=dltMsg onclick=module.dltMsg(" + data.key + ")>DELETE</button></div></div>";
+       messages.innerHTML += "<div style=justify-content:end class=outer id=" + data.key + "><div id=inner class=me>you : " + data.val().msg + "<button id=dltMsg onclick=module.dltMsg(" + data.key + ")>DELETE</button></div></div>";
    } else {
-      messages.innerHTML += "<div class=outer id=" + data.key + "><div id=inner class=notMe>" + data.val().sender + " : " + data.val().msg + "</div></div>";
+       messages.innerHTML += "<div class=outer id=" + data.key + "><div id=inner class=notMe>" + data.val().sender + " : " + data.val().msg + "</div></div>";
    }
-});
-
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
